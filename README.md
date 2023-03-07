7cf3ea751e0df09ee3bf907af1bc29df578c7d44
JoseMMorales <josemulamorales33@hotmail.com>
2023-03-07 11:38:11 +0100

    Unit Test with Jasmine and Karma in the dropdown

--- README.md
+++ README.md
@@ -12,6 +12,20 @@ Select developed to be used in any certain occasion that will be required to bui
 
 BehaviorSubject, fromEvent, Subscription
 
+### Unit Test Output...
+
+#### 0 Failures in 10 specs...
+
+<div align="center">
+  <img width="361" alt="jasmine" src="https://user-images.githubusercontent.com/43299285/223396990-b7ec5204-04bc-4aad-a1f8-5becbff037ba.PNG">
+</div>
+
+#### 100% Coverage...
+
+<div align="center">
+  <img width="533" alt="Coverage" src="https://user-images.githubusercontent.com/43299285/223398019-7220594a-d082-4808-aa1d-9ebcd5b126b2.PNG">
+</div>
+
 ### Technology
 
 <b>rxjs</b>: "7.5.0"</br>
--- src/app/drop-down/drop-down.component.spec.ts
+++ src/app/drop-down/drop-down.component.spec.ts
@@ -1,4 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { BehaviorSubject, of } from 'rxjs';
+import * as rxjs from 'rxjs';
 
 import { DropDownComponent } from './drop-down.component';
 
@@ -6,12 +9,22 @@ describe('DropDownComponent', () => {
   let component: DropDownComponent;
   let fixture: ComponentFixture<DropDownComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DropDownComponent ]
-    })
-    .compileComponents();
+  const dropdownStub: string[] = [
+    'Option 1',
+    'Option 2',
+    'Option 3',
+    'Option 4',
+    'Option 5',
+    'Option 6',
+  ];
 
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [DropDownComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(DropDownComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +32,88 @@ describe('DropDownComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('OnIninit', () => {
+    it('Should render default field when initialize the Dropdown', () => {
+      //Arrange
+      const defaultValue = dropdownStub[0];
+      const dropdownElement = fixture.debugElement.query(
+        By.css('.dropdown-value')
+      ).nativeElement.textContent;
+
+      // Act
+      component.ngOnInit();
+
+      // Assert
+      expect(dropdownElement).toEqual(defaultValue);
+      expect(component.currentValue).toEqual(defaultValue);
+    });
+  });
+
+  describe('dropdown functionalities', () => {
+    it('Should populate options in the dropdown', () => {
+      //Arrange
+      const dropdownOptions = fixture.debugElement.queryAll(By.css('li span'));
+
+      // Act
+      component.ngOnInit();
+
+      // Assert
+      dropdownOptions.forEach((option, i) => {
+        expect(option.nativeElement.textContent).toContain(dropdownStub[i]);
+      });
+      expect(dropdownOptions.length).toEqual(6);
+    });
+
+    it('Should close the dropdown when user choose a value', () => {
+      //Arrange
+      const valueSelected = 'Option 1';
+      const subject = new BehaviorSubject<boolean>(false);
+
+      //Act
+      component.select(valueSelected);
+
+      // Assert
+      expect(component.currentValue).toEqual('Option 1');
+      expect(component.dropdownOpen$.getValue()).toEqual(subject.getValue());
+    });
+
+    it('Should make toggle when dropdown value is false', () => {
+      //Arrange
+      const subject$ = new BehaviorSubject(false);
+
+      //Act
+      component.toggleDropdown();
+
+      //Assert
+      subject$.next(true);
+      expect(component.dropdownOpen$.getValue()).toEqual(subject$.getValue());
+    });
+
+    it('Should make toggle when dropdown value is true', () => {
+      //Arrange
+      const subject$ = new BehaviorSubject(false);
+
+      //Act
+      component.dropdownOpen$.next(true);
+      component.toggleDropdown();
+
+      //Assert
+      expect(component.dropdownOpen$.getValue()).toEqual(subject$.getValue());
+    });
+
+    it('Should close the dropdown when clicking outside', () => {
+      //Arrange
+      const subject$ = new BehaviorSubject(false);
+
+      //Act
+      component.ngOnInit();
+      document.dispatchEvent(new Event('click'));
+      fixture.detectChanges();
+
+      //Assert
+      expect(component.dropdownOpen$.getValue()).toEqual(subject$.getValue());
+    });
   });
 });
